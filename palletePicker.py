d74b3d0d38e396a9dc05c3f387d9654afde72646
likhithb <likhithborela@gmail.com>
2022-01-22 17:26:20 -0800

    implementing pickColorBottom with colormind api

--- palletePicker.py
+++ palletePicker.py
@@ -1,15 +1,35 @@
+import os
+import subprocess
 
 class PalletePicker:
 
     def __init__(self) -> None:
-        self.palleteDictionary = {}
-        self.startingColor = ''
+        self.topColor = '44,43,44'
+        self.bottomColor = ''
         self.seasons = ('winter', 'spring', 'summer', 'fall')
 
-    def pickColorPants(self, color):
-        
-        if(self.startingColor == ''):
-            return f'A color has not been chosen yet, please choose a color'
+    def pickColorBottom(self) -> None:
+
+        if(self.topColor == ''):
+            print(f'A color has not been chosen yet, please choose a color :)')
         else:
-            for i in self.palleteDictionary:
-                if 
+            os.system("curl \'http://colormind.io/api/\' --data-binary \'{\"input\":"+f"[[{self.getTopColor}]]"+",\"model\":\"default\"}\'")
+            
+
+    @property
+    def getBottomColor(self) -> str:
+        return self.bottomColor
+    
+    @property
+    def getTopColor(self) -> str:
+        return self.topColor
+    
+    
+
+
+
+##
+test = PalletePicker()
+print(test.getTopColor)
+test.pickColorBottom()
+##
